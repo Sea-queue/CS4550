2710a021e7f6a1c43427d4af1f320a0f4e1c99d2
Sea-queue <chengqian1214@gmail.com>
2024-01-19 19:03:05 -0500

    fixed the navigation style issues

--- public/kanbas/navigation/index.js
+++ public/kanbas/navigation/index.js
@@ -39,7 +39,7 @@ function KanbasNavigation(location) {
   ];
 
   document.write(`
-    <ul style="list-style-type: none; background-color:orange; margin:0px; padding-left:10px">
+    <ul style="width:100px; list-style-type: none; background-color:orange; margin:0px; padding-left:10px">
       ${links.map(
           (link) => `<li style="width:100%; padding:10px; ${location === link.name ? "border-left: 3px solid green;" : ""}">
                         <a href="${link.url}">${link.name}</a>
