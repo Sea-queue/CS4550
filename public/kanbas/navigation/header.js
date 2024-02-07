aab2f19ec4190c778ef2316bb18b8a5aaf1b9c19
Sea-queue <chengqian1214@gmail.com>
2024-02-06 19:33:45 -0500

    adding more to the header

--- public/kanbas/navigation/header.js
+++ public/kanbas/navigation/header.js
@@ -1,27 +1,115 @@
 function Header() {
+  const link1 = [
+    {
+      name:"Account",
+      url:"/kanbas/account/profile/screen.html",
+      icon: "fa fa-solid fa-user"
+    },
+    {
+      name:"Dashboard",
+      url:"/kanbas/dashboard/screen.html",
+      icon: "fa fa-tachometer"
+    },
+    {
+      name:"Courses",
+      url:"/kanbas/courses/home/screen.html",
+      icon: "fa fa-solid fa-bell"
+    },
+    {
+      name:"Calendar",
+      url:"",
+      icon:"fa fa-calendar"
+    },
+    {
+      name:"Inbox",
+      url:""
+    },
+    {
+      name:"History",
+      url:""
+    },
+    {
+      name:"Studio",
+      url:""
+    },
+    {
+      name:"Commons",
+      url:""
+    },
+    {
+      name:"Help",
+      url:""
+    },
+  ];
+
+  const link2 = [
+    {
+      name:"Home",
+      url:"/kanbas/courses/home/screen.html"
+    },
+    {
+      name:"Modules",
+      url:"/kanbas/courses/modules/screen.html"
+    },
+    {
+      name:"Piazza",
+      url:""
+    },
+    {
+      name:"Zoom",
+      url:""
+    },
+    {
+      name:"Assignments",
+      url:"/kanbas/courses/assignments/screen.html"
+    },
+    {
+      name:"Quizes",
+      url:""
+    },
+    {
+      name:"Grades",
+      url:"/kanbas/courses/grades/screen.html"
+    },
+    {
+      name:"People",
+      url:""
+    },
+  ];
+
   document.write(`
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">NEU</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+      <button class="navbar-toggler float-left" type="button" data-toggle="collapse" data-target="#kanbasNavigation" aria-controls="kanbasNavigation" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
+      <a class="navbar-brand" href="#">NEU</a>
+      <button class="navbar-toggler float-left" type="button" data-toggle="collapse" data-target="#courseNavigation" aria-controls="courseNavigation" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fa fa-solid fa-chevron-down"></i>
+      </button>
     
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
+      <div class="collapse navbar-collapse" id="kanbasNavigation">
+        <ul class="navbar-nav mr-auto header">
+          ${link1.map(
+            (link) => `<li class="nav-item">
+                          <a href="${link.url}">
+                            <i class="${link.icon}"></i>
+                            ${link.name}
+                          </a>
+                      </li>`
+          ).join("")}
+        </ul>
+      </div>
+
+      <div class="collapse navbar-collapse" id="courseNavigation">
+        <ul class="navbar-nav mr-auto header">
+          ${link2.map(
+            (link) => `<li class="nav-item">
+                          <a href="${link.url}">
+                            <i class="${link.icon}"></i>
+                            ${link.name}
+                          </a>
+                      </li>`
+          ).join("")}
         </ul>
       </div>
     </nav>
