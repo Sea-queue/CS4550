3e7f289b60ce551adf583c91b7a23a087d14365c
Sea-queue <chengqian1214@gmail.com>
2024-02-11 16:57:06 -0500

    re arranged landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+// const name = () => {}
+// name is an instance of the unnamed function, name's type is function it is not the function's name,
+// interpretor check's this const name's type, if it's a function, it allows it to take parameters.
+// it fixes "this" key words behave like other languages.
+
+// be careful with the syntax
+// function always returns
+// (a, b) => a - b, no need to explicitly type return,
+// (a, b) => {
+//   return a - b;
+// }
+// has to include the return.
+/*
+- how the JT and HTML can nested in each other smoothly in map example.
+- array list can take DOM type meaning html tags.
+- writing to real DOM is very expensive, JS is mataining a virtual DOM which needs unique key to keep track
+of the contents, So that it can compare the virtual and real DOM to only update the current changes.
+What do you use for auto correct format and copilot.
+
+<pre> tag convert it to Arial font. 
+JSON.stringify(todo, null, 2) 2 is the indentation.
+*/
 
 function App() {
-  return (
-    <h2>JS/TS</h2>
-  );
+  return <h2>JS/TS</h2>;
 }
 
 export default App;
