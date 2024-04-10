97577dd557c9d11e68ae1f7bf0022bc446558081
Sea-queue <chengqian1214@gmail.com>
2024-04-09 21:18:03 -0400

    get rid of irrelevant files

--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -3,11 +3,10 @@ import {
   FaTachometerAlt,
   FaBook,
   FaNapster,
-  FaYoutube,
+  FaTv,
   FaBrain,
   FaRegCalendarAlt,
   FaVideo,
-  FaInbox,
 } from "react-icons/fa";
 import "./index.css";
 import { Link, useLocation } from "react-router-dom";
@@ -17,8 +16,8 @@ function KanbasNavigation() {
   const links = [
     { label: "Account", icon: <FaRegUserCircle className="fs-2" /> },
     { label: "OpenAI", icon: <FaBrain className="fs-2" /> },
-    { label: "YouTube", icon: <FaYoutube className="fs-2" /> },
-    { label: "Napster", icon: <FaNapster className="fs-2" /> },
+    { label: "Movies", icon: <FaTv className="fs-2" /> },
+    { label: "Music", icon: <FaNapster className="fs-2" /> },
     { label: "Dashboard", icon: <FaTachometerAlt className="fs-2" /> },
     { label: "Courses", icon: <FaBook className="fs-2" /> },
     { label: "Calendar", icon: <FaRegCalendarAlt className="fs-2" /> },
